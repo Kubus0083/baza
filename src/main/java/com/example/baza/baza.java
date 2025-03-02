d908a23deb68f2f94e554dc05706d6842149a509
Kubus0083 <kubus.sz2008@gmail.com>
2025-03-02 20:24:13 +0100

    Zsdfgsdgsdgadasdasd

--- src/main/java/com/example/baza/baza.java
+++ src/main/java/com/example/baza/baza.java
@@ -7,7 +7,7 @@ import java.sql.*;
 
 public class baza {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/school3tf";
+    private static final String URL = "jdbc:mysql://localhost:3306/school";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
